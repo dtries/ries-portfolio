dcb631c3e52667b162cc49baf42dd886fc43c0ba
Dennis Ries <dtries@gmail.com>
2020-02-06 13:19:22 -0500

    Fixed Navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,16 +3,18 @@ import React, {Component} from 'react';
 class Navigation extends Component {
     render () {
         return (
-        <nav>
-            <div className="nav-wrapper">
-                <a href="/" className="brand-logo left">Logo</a>
-                <ul id="nav-mobile" class="right hide-on-med-and-down">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/Portfolio">Portfolio</a></li>
-                    <li><a href="/About">About</a></li>
-                </ul>
-            </div>
-        </nav> 
+        <div className="navbar-fixed">    
+            <nav>
+                <div className="nav-wrapper">
+                    <a href="/" className="brand-logo left">Logo</a>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/Portfolio">Portfolio</a></li>
+                        <li><a href="/About">About</a></li>
+                    </ul>
+                </div>
+            </nav> 
+        </div>
         );       
     }
 }
