496812f5dc4460caa7e9e06a7c7f160ca70af191
Dennis Ries <dtries@gmail.com>
2020-02-06 12:41:57 -0500

    build up heroku 8

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route} from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import PortoflioPage from './components/PortfolioPage';
 import AboutPage from './components/AboutPage';
+import Navigation from './components/Navigation';
 // import logo from './logo.svg';
 import './App.css';
 
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <Navigation />
         <Route exact path="/" component={LandingPage} />
         <Route path="/Portfolio" component={PortoflioPage} />
         <Route path="/About" component={AboutPage} />
