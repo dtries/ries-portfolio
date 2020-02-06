5ff8b0ab9228a8ec88cffc1902d6fc3c2f497e56
Dennis Ries <dtries@gmail.com>
2020-02-06 12:09:31 -0500

    build up heroku 5

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Route exact path="/" component={LandingPage} />
+      <Router>
+        <Route exact path="/" component={LandingPage} />
+      </Router>
     </div>
   );
 }
